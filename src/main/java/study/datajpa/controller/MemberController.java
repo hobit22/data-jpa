3c40bbfd3a42654a04ed346134165427c9267fe2
hobit22 <hobit22@naver.com>
2024-11-15 16:24:28 +0900

    [ADD] 도메인 클래스 컨버터

---
+++ src/main/java/study/datajpa/controller/MemberController.java
@@ -0,0 +1,33 @@
+package study.datajpa.controller;
+
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import study.datajpa.entity.Member;
+import study.datajpa.repository.MemberRepository;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberRepository memberRepository;
+
+    @GetMapping("/members/{id}")
+    public String findMember(@PathVariable Long id) {
+        Member member = memberRepository.findById(id).get();
+        return member.getUsername();
+    }
+
+
+    @GetMapping("/members2/{id}") // 권장하진 않음. 복잡해지면 사용할 수 없음. transaction 이 없기 때문에 조회용으로만 사용해야함.
+    public String findMember(@PathVariable("id") Member member) {
+        return member.getUsername();
+    }
+
+    @PostConstruct
+    public void init() {
+        memberRepository.save(new Member("test"));
+    }
+}
